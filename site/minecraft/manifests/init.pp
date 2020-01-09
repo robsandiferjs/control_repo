78c7cef2b22fc8ff95de331a5f6ce2c98e842e00
robsandiferjs <59240538+robsandiferjs@users.noreply.github.com>
2020-01-09 12:50:53 -0600

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,28 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar'
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+  service {'minecraft':
+    ensure => running,
+    enable => true,
+  }
+} 
+
+
+
+
